4bc70ef9cacab4c2ad2389eb4a402c5531192ac2
Shivangi <shivangit1111@gmail.com>
2023-05-10 21:28:55 +0530

    feat: create dataBase and getData

--- app/src/main/java/com/example/quizapp/FragmentA.kt
+++ app/src/main/java/com/example/quizapp/FragmentA.kt
@@ -1,25 +1,69 @@
 package com.example.quizapp
 
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.room.Room
+import com.example.quizapp.database.QuestionDataBase
+import com.example.quizapp.databinding.FragmentABinding
+import com.example.quizapp.models.Questions
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
 class FragmentA : Fragment() {
 
 
+    private lateinit var binding: FragmentABinding
+    // create string for question, answer and options
+
+    lateinit var dataBase: QuestionDataBase
+    private var currentQuestionIndex = 0
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        binding = FragmentABinding.inflate(inflater, container, false)
 
-        val view = inflater.inflate(R.layout.fragment_a, container, false)
-        // Inflate the layout for this fragment
+        dataBase = Room.databaseBuilder(requireContext(), QuestionDataBase::class.java, "questionDatabase").fallbackToDestructiveMigration().build()
 
-        return view
+        GlobalScope.launch {
+            dataBase.questionDao().addQuestion(Questions(0,"Tell Name","savita","BBli","Pinky","Sonum","Sonum"))
+
+        }
+
+        getData(binding.root)
+        return binding.root
+
+
     }
 
+    fun getData(view: View){
+
+        dataBase.questionDao().getAllQuestions().observe(viewLifecycleOwner, Observer {
+                questions ->
+            if (questions.isNotEmpty()) {
+                val currentQuestion = questions[currentQuestionIndex]
+                binding.tvQuestion.text = currentQuestion.questionText
+                binding.radioButton1.text = currentQuestion.option1
+                binding.radioButton2.text = currentQuestion.option2
+                binding.radioButton3.text = currentQuestion.option3
+                binding.radioButton4.text = currentQuestion.option4
+            }
+
+
+
+        })
+        binding.nextQuestionBtn.setOnClickListener {
+            currentQuestionIndex++
+            getData(it)
+        }
+
+    }
 
 }
