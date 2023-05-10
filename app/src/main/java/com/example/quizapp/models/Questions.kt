88fddbb01120fa67c56bb5053b71a8bdfed90e32
Shivangi <shivangit1111@gmail.com>
2023-05-10 21:18:38 +0530

    feat: create data class

--- app/src/main/java/com/example/quizapp/models/Questions.kt
+++ app/src/main/java/com/example/quizapp/models/Questions.kt
@@ -1,4 +1,4 @@
-package com.example.quizapp
+package com.example.quizapp.models
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "questions")
 data class Questions(
     @PrimaryKey(autoGenerate = true)
-    val id: Long = 0,
+    val id:Int,
     val questionText: String,
     val option1: String,
     val option2: String,
