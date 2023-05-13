7fecc12ab621771137d72551832529b1de837f7d
Shivangi <shivangit1111@gmail.com>
2023-05-13 15:49:04 +0530

    feat: Play again

--- app/src/main/java/com/example/quizapp/FragmentB.kt
+++ app/src/main/java/com/example/quizapp/FragmentB.kt
@@ -1,5 +1,6 @@
 package com.example.quizapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,14 +12,36 @@ class FragmentB : Fragment() {
 
 
     private lateinit var binding: FragmentBBinding
+
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
 
         binding = FragmentBBinding.inflate(inflater, container, false)
-        // Inflate the layout for this fragment
+
+        val totalQuestions = arguments?.getInt("totalQuestions", 0)
+        val score = arguments?.getInt("Score", 0) ?: 0
+
+        binding.getScore.text = "You have scored:$score,\n out of:$totalQuestions"
+
+        binding.btnStartAgain.setOnClickListener {
+            fragmentA()
+        }
         return binding.root
+
+    }
+
+    private fun fragmentA() {
+        val fragmentA = FragmentA()
+        val fragmentManager = requireActivity().supportFragmentManager
+
+        val transaction = fragmentManager.beginTransaction()
+
+        transaction.replace(R.id.container, fragmentA)
+        transaction.commit()
+
     }
 
 
